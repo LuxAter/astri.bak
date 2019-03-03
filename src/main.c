72b2e1af8dc933afbde64e036f7cb0f1757d8ed1
Arden Rasmussen <ardenisthebest@gmail.com>
2019-03-02 17:38:02 -0800

    Inital commit

---
+++ src/main.c
@@ -0,0 +1,56 @@
+#include <stdbool.h>
+
+#include "glad/glad.h"
+
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+  GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "Astri",
+                                        glfwGetPrimaryMonitor(), NULL);
+  if (window == NULL) {
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    // input
+    process_input(window);
+
+    // rendering
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
